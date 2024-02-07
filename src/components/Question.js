797843cde2b0f30188ce08af260acd3a62e3a5b9
Rotich <jane.rotich@student.moringaschool.com>
2024-02-07 12:34:05 +0300

    Finish Test

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      const timerId = setTimeout(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+
+      // Cleanup function to clear the timeout when the component is unmounted or when time is up
+      return () => clearTimeout(timerId);
+    } else {
+      // Time is up, trigger onAnswered with false
+      onAnswered(false);
+    }
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
+    // Reset the timer when an answer is chosen
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
